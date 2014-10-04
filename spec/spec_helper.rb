808a7e81f156647ff94e4f8238545096b33c9aca
nilsding <nilsding@nilsding.org>
2014-10-04 22:53:20 +0200

    added footnotes to the extensions list

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,8 @@ module Helpers
     # we actually have to create a new renderer each time, or else redcarpet
     # fails after about four tests.
     # see also https://github.com/vmg/redcarpet/issues/318
-    renderer = Redcarpet::Markdown.new(LaTeXRenderer, fenced_code_blocks: true)
+    renderer = Redcarpet::Markdown.new(LaTeXRenderer, fenced_code_blocks: true,
+                                       footnotes: true)
     renderer.render md
   end
 end
